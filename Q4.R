7f4cb18c168a709fe1c6723ddce46949418d7e62
amartsa <50933622+amartsa@users.noreply.github.com>
2020-04-14 17:05:50 -0300

    Delete Q4.R

--- Q4.R
+++
@@ -1,5 +0,0 @@
-myVar<-heptathlon
-myVar[,4]=NULL #Repeated five times to delete the columns we won't be using
-myFake<-mvrnorm(50,c(mean(myVar$hurdles),mean(myVar$highjump),mean(myVar$shot)),var(myVar)) #saves the simulation in the "myFake" variable
-mFkdist<-mahalanobis(myFake,colMeans(myFake),var(myFake))
-qqPlot(mFkdist, dist="chisq", df=7)
